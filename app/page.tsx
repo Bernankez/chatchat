5eb01999a22160fc8f171bad5f6269702ec2aed9
cole <bernankeic@gmail.com>
2023-12-12 18:13:54 +0800

    feat: update styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,45 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { Icon } from "@iconify/react";
 import {
   DropdownMenuContent,
-  DropdownMenuLabel,
   DropdownMenuRadioGroup,
   DropdownMenuRadioItem,
-  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { DropdownMenu } from "@radix-ui/react-dropdown-menu";
 import { useState } from "react";
+import { modelList } from "@/lib/const";
 
 export default function Home() {
-  const [model, setModel] = useState("");
+  const [model, setModel] = useState(modelList[0]);
 
   return (
     <main className="min-h-[100dvh] py-24 px-12 max-w-[70ch] mx-auto">
       <div>
         <div>
-          <div>icon</div>
+          <Icon icon="fluent-emoji:robot" width="3.5rem" />
           <div></div>
         </div>
-        <div className="font-bold text-4xl">chatchat</div>
+        <div className="font-bold text-4xl bg-gradient-to-r from-0% from-orange-500 to-30% to-orange-300 bg-clip-text text-transparent">
+          chatchat
+        </div>
         <div className="flex items-center gap-3">
           <div className="text-muted-foreground shrink-0">Based on OpenAI API</div>
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
-              <Button variant="ghost">Open</Button>
+              <Button variant="ghost" className="text-muted-foreground">
+                {model}
+              </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent className="w-56">
               <DropdownMenuRadioGroup value={model} onValueChange={setModel}>
-                <DropdownMenuRadioItem value="top">gpt-4-1106-preview</DropdownMenuRadioItem>
+                {modelList.map((model) => (
+                  <DropdownMenuRadioItem value={model} key={model}>
+                    {model}
+                  </DropdownMenuRadioItem>
+                ))}
               </DropdownMenuRadioGroup>
             </DropdownMenuContent>
           </DropdownMenu>
---
+++ lib/const.ts
@@ -0,0 +1,1 @@
+export const modelList = ["gpt-4-1106-preview"];
