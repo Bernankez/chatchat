75fd036bbef11dec5e4bcc67b2a713911401dd7f
cole <bernankez@qq.com>
2023-12-13 23:53:04 +0800

    feat: replace space-* with gap

--- components/common/settings-panel.tsx
+++ components/common/settings-panel.tsx
@@ -14,14 +14,14 @@ export default function SettingsPanel(props: SettingsPanelProps) {
 
   return (
     <div className={className} style={style}>
-      <div className="flex flex-col space-y-2 text-center sm:text-start">
+      <div className="flex flex-col gap-2 text-center sm:text-start">
         <div className="text-lg font-semibold text-foreground">Edit profile</div>
         <div className="text-sm text-muted-foreground">
           Make changes to your profile here. Click save when you are done.
         </div>
       </div>
       <div className="grid gap-4 py-4"></div>
-      <div className="flex flex-col-reverse sm:flex-row justify-end space-x-2">
+      <div className="flex flex-col-reverse sm:flex-row justify-end gap-2">
         <Button type="submit">Save changes</Button>
       </div>
     </div>
