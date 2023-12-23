5b7ed483660573049b9152d227d97ae26e9e5588
cole <bernankeic@gmail.com>
2023-12-24 01:06:35 +0800

    wip: settings panel

--- components/settings/settings-panel.tsx
+++ components/settings/settings-panel.tsx
@@ -1,6 +1,9 @@
 import { CSSProperties } from "react";
 import { Button } from "../ui/button";
 import { useSettingsStore } from "@/store/settings-store";
+import { useTranslation } from "react-i18next";
+import { Switch } from "../ui/switch";
+import { Label } from "../ui/label";
 
 export interface SettingsPanelProps {
   style?: CSSProperties;
@@ -10,6 +13,7 @@ export interface SettingsPanelProps {
 
 export default function SettingsPanel(props: SettingsPanelProps) {
   const { onClose, className, style } = props;
+  const { t } = useTranslation("settings");
 
   return (
     <div className={className} style={style}>
@@ -26,6 +30,16 @@ export default function SettingsPanel(props: SettingsPanelProps) {
       <div className="grid gap-4 py-4"></div>
       <div className="flex flex-col-reverse sm:flex-row justify-end gap-2">
         <Button type="submit">Save changes</Button>
+      </div>
+      <div className="flex flex-col gap-2 text-center sm:text-start">
+        <div className="text-lg font-semibold text-foreground">{t("panel.interfaceSettings")}</div>
+      </div>
+      <div className="grid gap-4 py-4">
+        TODO upgrade shadcn
+        <Label>
+          Swap wrap and send
+          <Switch></Switch>
+        </Label>
       </div>
     </div>
   );
