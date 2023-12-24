e944d796a9fda0514efe6624c91349ed8e981ee4
cole <bernankeic@gmail.com>
2023-12-25 01:16:08 +0800

    feat: Footer placeholder

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,6 +3,8 @@ import ChatInputArea from "@/components/chat/chat-input-area";
 import { createContext } from "react";
 import { useHistoryTravel } from "ahooks";
 import { useSettingsStore } from "@/store/settings-store";
+import Placeholder from "@/components/ui/placeholder";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export const ChatInputAreaContext = createContext({
   back: () => {},
@@ -17,11 +19,20 @@ export default function Footer() {
 
   return (
     <ChatInputAreaContext.Provider value={{ back, forward }}>
-      {useSimpleInput ? (
-        <ChatInputAreaSimple value={state} onInput={setValue}></ChatInputAreaSimple>
-      ) : (
-        <ChatInputArea value={state} onInput={setValue}></ChatInputArea>
-      )}
+      <Placeholder
+        skeleton={
+          <div className="flex gap-3">
+            <Skeleton className="w-full h-14"></Skeleton>
+            <Skeleton className="shrink-0 w-14 h-14"></Skeleton>
+            <Skeleton className="shrink-0 w-14 h-14"></Skeleton>
+          </div>
+        }>
+        {useSimpleInput ? (
+          <ChatInputAreaSimple value={state} onInput={setValue}></ChatInputAreaSimple>
+        ) : (
+          <ChatInputArea value={state} onInput={setValue}></ChatInputArea>
+        )}
+      </Placeholder>
     </ChatInputAreaContext.Provider>
   );
 }
